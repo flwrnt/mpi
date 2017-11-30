037536bad39fa28b5321fca2584fbb32540e57a0
FlwRnt <florent.auplat@etu.univ-tours.fr>
2017-11-30 08:58:45 -0500

    add comment & send only some line from left matrix

--- d3.c
+++ d3.c
@@ -1,16 +1,18 @@
 #include <mpi.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-typedef struct Matrix_
-{
+typedef struct Matrix_ {
 	int rows;
 	int cols;
 	int* data;
 } Matrix;
 
-Matrix createMatrix(int size)
-{
+/**
+ * create matrix & allocate memory 
+ */
+Matrix createMatrix(int size) {
 	Matrix result;
 	result.rows = size;
 	result.cols = size;
@@ -21,8 +23,10 @@ Matrix createMatrix(int size)
 	return result;
 }
 
-MPI_Datatype createLine(int cols)
-{
+/**
+ * create Line type
+ */
+MPI_Datatype createLine(int cols) {
 	MPI_Datatype result;
 	MPI_Type_contiguous(cols, MPI_INT, &result);
 	MPI_Type_commit(&result);
@@ -30,9 +34,13 @@ MPI_Datatype createLine(int cols)
 	return result;
 }
 
+/**
+ * init left and right matrix from stdin
+ */
 int init(Matrix *left, Matrix *right) {
 	int size, i = 0;
-  	char c;
+	char c;
+
 	fscanf(stdin, "%d", &size);
 
 	*left = createMatrix(size);
@@ -42,15 +50,15 @@ int init(Matrix *left, Matrix *right) {
     if (c == '\n') break;
 		i++;
   }
-
 	i = 0;
-	while (fscanf(stdin, "%d", &right->data[i]) != EOF) {
-		i++;
-  }
+	while (fscanf(stdin, "%d", &right->data[i]) != EOF) i++;
 
 	return size;
 }
 
+/**
+ * display matrix 
+ */
 void displayMatrix(Matrix *matrix){
 	printf("[ ");
 	for (int i=0; i<matrix->cols*matrix->rows; i++) 
@@ -58,7 +66,7 @@ void displayMatrix(Matrix *matrix){
 	printf("]\n");
 }
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv) {
 
  	MPI_Init(&argc, &argv);
 
@@ -66,16 +74,26 @@ int main(int argc, char** argv) {
 	MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 	MPI_Comm_rank(MPI_COMM_WORLD, &wrank);
 
-    Matrix left, right, res;
-	int n, range, rest = 0;
+	Matrix left, right, final;
+	int n, mult, rest = 0, display = 0, serial = 0;
 	int executionArray[world_size];
 	int displs[world_size];
-	double Tbegin, Tend;
+	double begin;
 
 	if (wrank == 0) {
+		// get -p option to display matrix
+		for (int i=0; i<argc; i++) {
+			if (strchr(argv[i], '-') && strchr(argv[i], 'p'))
+				display = 1;
+			if (strchr(argv[i], '-') && strchr(argv[i], 's'))
+				serial = 1;
+		}
+		// init left, right & result matrices
 		n = init(&left, &right);
+		final = createMatrix(n);
 	}
 
+	// broadcast size to other process
 	MPI_Bcast(&n, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
 	if (world_size > n) {
@@ -84,91 +102,90 @@ int main(int argc, char** argv) {
 		return 0;
 	}
 
-	Tbegin = MPI_Wtime(); 
+	begin = MPI_Wtime(); 
 
-	range = n / world_size;
+	// get line count by process
+	mult = n / world_size;
+	// create Line MPI type
 	MPI_Datatype Line = createLine(n);
 
 	if (wrank != 0) {
+		// allocate matrices for other processes
 		left = createMatrix(n);
 		right = createMatrix(n);
 	}
 	else{
+		// calc rest && fill execution & offset arrays
 		rest = n % world_size;
 		
-		displs[0] = 0;
-		executionArray[0] = range;
-		
-		if(0 < rest)
-			executionArray[0] += 1;
-
-		for(int i = 1; i < world_size; i++){
-			executionArray[i] = range;
+		for(int i = 0; i < world_size; i++){
+			executionArray[i] = mult;
 
 			if(i < rest)
 				executionArray[i] += 1;
 
-			displs[i] =  displs[i - 1] + executionArray[i - 1];
+			displs[i] = i==0 ? 0 : displs[i-1] + executionArray[i-1];
 		}
 	}
 
+	// broadcast execution & offset arrays
 	MPI_Bcast(&executionArray, world_size, MPI_INT, 0, MPI_COMM_WORLD);
 	MPI_Bcast(&displs, world_size, MPI_INT, 0, MPI_COMM_WORLD);
 
-	res = createMatrix(n);
+	int * leftLines = (int*) malloc(executionArray[wrank]*n*sizeof(int));
+	MPI_Scatterv(left.data, executionArray, displs, Line, leftLines, executionArray[wrank], Line, 0, MPI_COMM_WORLD);
 
-	MPI_Bcast(left.data, n, Line, 0, MPI_COMM_WORLD);
+	// broadcast right matrix
 	MPI_Bcast(right.data, n, Line, 0, MPI_COMM_WORLD);
 
-	int * resultLine = (int*) malloc(n*n*sizeof(int));
+	// allocate result lines for each process
+	int * resultLine = (int*) malloc(executionArray[wrank]*n*sizeof(int));
 	int index = 0;
 
+	// calculate lines by process
 	for (int j=0; j<executionArray[wrank]; j++) {
-		for (int col=0; col<left.cols; ++col, ++index)
-		{
-			int result = 0;
+		for (int col=0; col<left.cols; ++col, ++index) {
+			int tmp = 0;
 			for (int i=0; i<left.rows; ++i)
-				result += left.data[(displs[wrank]+j)*left.cols + i] * right.data[col + i*left.cols];
+				tmp += leftLines[j*left.rows + i] * right.data[col + i*left.cols];
 
-			resultLine[index] = result;
+			resultLine[index] = tmp;
 		}
 	}
 
-	MPI_Gatherv(resultLine, executionArray[wrank], Line, res.data, executionArray, displs, Line, 0, MPI_COMM_WORLD);
-	Tend = MPI_Wtime(); 
+	// send back all results lines to process 0
+	MPI_Gatherv(resultLine, executionArray[wrank], Line, final.data, executionArray, displs, Line, 0, MPI_COMM_WORLD);
 
 	if (wrank == 0) {
-		printf("Parallel : %f\n", Tend - Tbegin);
-		// displayMatrix(&res);
+		fprintf(stderr, "Parallel: \t%f sec\n", MPI_Wtime() - begin);
+		if (display) displayMatrix(&final);
+		 	if (serial) {
+				// execute serial version to compare
+				Matrix result = createMatrix(n);
+				begin = MPI_Wtime();
+				for (int row=0; row<left.rows; ++row) {
+					for (int col=0; col<left.cols; ++col) {
+						int tmp = 0;
+						for (int i=0; i<left.rows; ++i)
+							tmp += left.data[row*left.cols + i] * right.data[col + i*right.cols];
 
-		Matrix result = createMatrix(n);
-		Tbegin = MPI_Wtime();
-		for (int row=0; row<left.rows; ++row)
-		{
-			for (int col=0; col<left.cols; ++col)
-			{
-				int one = 0;
-				for (int i=0; i<left.rows; ++i)
-					one += left.data[row*left.cols + i] * right.data[col + i*right.cols];
+						result.data[row*result.cols + col] = tmp;
+					}
+				}
+				fprintf(stderr, "Serial: \t%f sec\n", MPI_Wtime()-begin);
 
-				result.data[row*result.cols + col] = one;
-			}
-		}
-
-		printf("Serial : %f\n", MPI_Wtime()-Tbegin);
-
-		// Comparaison des résultats.
-		int success = 1;
-		for (int i=0; i<result.rows*result.cols; ++i) {
-			if (res.data[i] != result.data[i]) {
-				success = 0;
-				break;
-			}
-		}
-		printf("Succès ? %d.\n", success);
-
-		free(res.data);
-		free(result.data);
+				// compare results
+				int success = 1;
+				for (int i=0; i<result.rows*result.cols; ++i) {
+					if (final.data[i] != result.data[i]) {
+						success = 0;
+						break;
+					}
+				}
+				fprintf(stderr, "Success: \t%s\n", success ? "true" : "false");
+				free(result.data);
+		 	}
+		free(final.data);
 	}
 	MPI_Type_free(&Line);
 	free(left.data);
